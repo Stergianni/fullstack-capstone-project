ebb859209d83ee81e8dacc9bdde5113ca21eb852
Stergianni <stella.orfanidis@hotmail.com>
2025-03-16 14:21:17 +0100

    Step 3: Configure the /api/auth endpoint

--- giftlink-backend/app.js
+++ giftlink-backend/app.js
@@ -5,47 +5,46 @@ const cors = require('cors');
 const pinoLogger = require('./logger');
 
 const connectToDatabase = require('./models/db');
-const {loadData} = require("./util/import-mongo/index");
+const { loadData } = require("./util/import-mongo/index");
 
 const app = express();
-app.use("*", cors()); // Enable CORS for all routes
+app.use("*", cors());
 const port = 3060;
 
-// Connect to MongoDB
+// Connect to MongoDB; we just do this one time
 connectToDatabase()
-  .then(() => {
-    pinoLogger.info('Connected to DB');
-  })
-  .catch((e) => console.error('Failed to connect to DB', e));
+    .then(() => {
+        pinoLogger.info('Connected to DB');
+    })
+    .catch((e) => console.error('Failed to connect to DB', e));
 
-app.use(express.json()); // Parse JSON request bodies
+app.use(express.json());
 
-// Import the routes
+// Route files
 const giftRoutes = require('./routes/giftRoutes');
 const searchRoutes = require('./routes/searchRoutes');
-const authRoutes = require('./routes/authRoutes'); // Import auth routes
+const authRoutes = require('./routes/authRoutes'); // Import the authRoutes
 
 const pinoHttp = require('pino-http');
 const logger = require('./logger');
 
 app.use(pinoHttp({ logger }));
 
-// Use the routes
+// Use Routes
 app.use('/api/gifts', giftRoutes);
 app.use('/api/search', searchRoutes);
-app.use('/api/auth', authRoutes); // Add the auth routes
+app.use('/api/auth', authRoutes); // Use authRoutes for /api/auth
 
 // Global Error Handler
 app.use((err, req, res, next) => {
-  console.error(err);
-  res.status(500).send('Internal Server Error');
+    console.error(err);
+    res.status(500).send('Internal Server Error');
 });
 
-// Test route
 app.get("/", (req, res) => {
-  res.send("Inside the server");
+    res.send("Inside the server");
 });
 
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+    console.log(`Server running on port ${port}`);
 });
